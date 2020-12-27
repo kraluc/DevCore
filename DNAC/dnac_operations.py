45256cef9b0a742d8dd5a52075b1635556b93cf9
Vincent Ricci <riccivm@gmail.com>
2020-12-27 14:05:39 -0800

    initial commit

---
+++ DNAC/dnac_operations.py
@@ -0,0 +1,35 @@
+#  Leverage always-on DNA sandbox to issue requests commands
+import requests
+
+def get_token():
+    """
+    Gets an access token from Cisco DNA Center always-on sandbox. Returns the token
+    string if successful; False otherwise
+    """
+
+    # Declare Useful local variabales to simplify request process
+    api_path = "https://sandboxdnac.cisco.com/dna"
+    auth = ("devnetuser", "Cisco123!")
+    headers = {"Content-type": "application/json"}
+
+    # Issue http POST request to the proper URL to request a token
+    auth_resp = requests.post(
+        f"{api_path}/system/api/v1/auth/token", auth=auth, headers=headers
+    )
+
+    # If successful, print token. Else, raise HTTPError with details
+    auth_resp.raise_for_status()
+    token = auth_resp.json()["Token"]
+    return token
+
+def main():
+    """
+    Execution begins here
+    """
+
+    token = get_token()
+    print(token)
+
+
+if __name__ == "__main__":
+    main()
