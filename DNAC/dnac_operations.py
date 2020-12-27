277b030bfeccf0f0fcc44e27ccf357df874175c9
Vincent Ricci <riccivm@gmail.com>
2020-12-27 15:49:27 -0800

    updated packages and logging

--- DNAC/dnac_operations.py
+++ DNAC/dnac_operations.py
@@ -6,28 +6,66 @@
 #       Go to https://sandboxdnac2.cisco.com
 #       Login with credentials [devnetuser/Cisco123!]
 
-import requests
+#!/bin/env python
 
-def get_token():
+import logging, sys
+import requests
+from requests.models import HTTPError
+
+
+#  Logging Parameters
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+#    stream=sys.stdout,
+    filename=f"{__file__.split('.')[0]}.log",
+    filemode='a',
+    level=logging.INFO
+    )
+logger = logging.getLogger(__name__)
+
+DEBUG = False
+if DEBUG:
+    logger.setLevel(level=logging.DEBUG)
+else:
+    # attribute level expects an integer --> logging.INFO
+    # >>> type(logging.info)
+    # <type 'function'>
+    # >>> type(logging.INFO)
+    # <type 'int'>
+    logger.setLevel(level=logging.INFO)
+
+# Default Credentials as published on https://sandboxdnac2.cisco.com
+AUTH = ("devnetuser", "Cisco123!")
+
+
+def get_token(username: str = AUTH[0], password: str = AUTH[1]):
     """
     Gets an access token from Cisco DNA Center always-on sandbox. Returns the token
-    string if successful; False otherwise
+    string if successful; False (None) otherwise
     """
 
     # Declare Useful local variabales to simplify request process
     api_path = "https://sandboxdnac.cisco.com/dna"
-    auth = ("devnetuser", "Cisco123!")
+    auth = (username, password)
+    #
     headers = {"Content-type": "application/json"}
 
     # Issue http POST request to the proper URL to request a token
-    auth_resp = requests.post(
-        f"{api_path}/system/api/v1/auth/token", auth=auth, headers=headers
-    )
-
     # If successful, print token. Else, raise HTTPError with details
-    auth_resp.raise_for_status()
-    token = auth_resp.json()["Token"]
-    return token
+    try:
+        auth_resp = requests.post(
+            f"{api_path}/system/api/v1/auth/token", auth=auth, headers=headers
+        )
+        auth_resp.raise_for_status()
+        logger.info(msg=f"Successful POST - {auth_resp.status_code}")
+        token = auth_resp.json()["Token"]
+        return token
+
+    except HTTPError as err:
+        logger.error(msg=err)
+        raise err
+
 
 def main():
     """
@@ -36,6 +74,7 @@ def main():
 
     token = get_token()
     print(token)
+    print(len(token))
 
 
 if __name__ == "__main__":
